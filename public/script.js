b77fa47568861c8b76d4920bfebf475616a2a9bd
Laney Dunker <lddunke@ilstu.edu>
2026-04-13 18:52:21 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,18 @@
+async function sendMessage() {
+  const input = document.getElementById("message").value;
+
+  const res = await fetch("/api/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      message: input
+    })
+  });
+
+  const data = await res.json();
+
+  document.getElementById("response").innerText = data.reply;
+}
 
