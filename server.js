ab4cecec6d096f764acebc472869f5e0f532202c
Laney Dunker <lddunke@ilstu.edu>
2026-04-13 18:51:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+import express from "express";
+import path from "path";
+import chatRoute from "./routes/chat.js";
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.static("public"));
+
+// Debug: log ALL requests
+app.use((req, res, next) => {
+  console.log(`📥 ${req.method} ${req.url}`);
+  next();
+});
+
+// Routes
+app.use("/api/chat", chatRoute);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
